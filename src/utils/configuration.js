3ba153629b25180d839f247f5393ceb3fad7c8c7
anzhang <zhangan@anzhangs-MacBook-Pro.local>
2022-06-02 23:46:45 -0700

    fix contract

--- src/utils/configuration.js
+++ src/utils/configuration.js
@@ -1,12 +1,12 @@
 export const CONTRACTADDRESS = {
     1: "",
-    4: "0x221AfC0c1fe4772ab767A0174AA43a6B3F24EF62",
+    4: "",
 };
 
 export const PRICE = 0;
 
 export const MAX_AMOUNT = 1000;
 
-export const MAX_BATCH_SIZE = 10;
+export const MAX_BATCH_SIZE = 9;
 
 export const OPENSEA_URL = "https://opensea.io/collection/"
